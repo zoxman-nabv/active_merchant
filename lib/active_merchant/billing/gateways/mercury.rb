f130f20bb4505f203be35ddfedfe8296e3f07f4e
andrewpaliga <andrew.paliga@shopify.com>
2014-12-03 10:50:46 -0500

    Revert "Switched all voids to reversals and updated MID"

--- lib/active_merchant/billing/gateways/mercury.rb
+++ lib/active_merchant/billing/gateways/mercury.rb
@@ -72,6 +72,13 @@ module ActiveMerchant #:nodoc:
       def void(authorization, options={})
         requires!(options, :credit_card) unless @use_tokenization
 
+        if options[:try_reversal]
+          request = build_authorized_request('VoidSale', nil, authorization, options[:credit_card], options.merge(:reversal => true))
+          response = commit('VoidSale', request)
+
+          return response if response.success?
+        end
+
         request = build_authorized_request('VoidSale', nil, authorization, options[:credit_card], options)
         commit('VoidSale', request)
       end
@@ -108,7 +115,7 @@ module ActiveMerchant #:nodoc:
         xml = Builder::XmlMarkup.new
 
         invoice_no, ref_no, auth_code, acq_ref_data, process_data, record_no, amount = split_authorization(authorization)
-        ref_no = "1" if ref_no.blank?
+        ref_no = "1" if options[:reversal] #filler value for preauth voids -- not used by mercury but will reject if missing or not numeric
 
         xml.tag! "TStream" do
           xml.tag! "Transaction" do
@@ -125,8 +132,8 @@ module ActiveMerchant #:nodoc:
             add_address(xml, options)
             xml.tag! 'TranInfo' do
               xml.tag! "AuthCode", auth_code
-              xml.tag! "AcqRefData", acq_ref_data
-              xml.tag! "ProcessData", process_data 
+              xml.tag! "AcqRefData", acq_ref_data if options[:reversal]
+              xml.tag! "ProcessData", process_data if options[:reversal]
             end
           end
         end
